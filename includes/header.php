e4f4d262488c2249da8f633d2606d566f3061de6
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-12-30 09:49:21 +0530

    expertise updated

--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,7 @@
     <title>My Portfolio</title>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="css/expertise.css">
-    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
 <script>
     AOS.init();
 </script>
